69391515c3e2f52fb248a9f850ac8c95b36507f3
Muzamil Ali <105632991+Muzmmil207@users.noreply.github.com>
2023-04-10 02:53:44 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
-
-poetry install
-poetry add gunicorn
-
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate

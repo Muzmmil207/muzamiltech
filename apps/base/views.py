fcec6f39ec2980d6368faa44c47122633596f7ae
mozmmil-ali <mly88207@gmail.com>
2023-04-12 02:58:04 +0200

    added about & contact pages

--- apps/base/views.py
+++ apps/base/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 
 
 def home(request):
-
     return render(request, "apps/base/home.html")
+
+
+def about(request):
+    return render(request, "apps/base/about.html")
+
+
+def contact(request):
+    return render(request, "apps/base/contact.html")

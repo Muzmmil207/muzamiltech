362c07765d9657e238534e6add93d8ca783b7497
mozmmil-ali <mly88207@gmail.com>
2023-04-24 19:51:06 +0200

    createtd robots.txt file

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.flatpages.views import flatpage
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 from . import views
 from .sitemaps import ArticleSitemap
@@ -14,8 +15,9 @@ urlpatterns = [
     path("contact", views.contact, name="contact"),
     path("contact-handler", views.contact_handler),
     path("subscribe-handler", views.subscribe_handler),
-    # sitemap
+    # Search engine crawlers stuff
     path(
         "sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"
     ),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
